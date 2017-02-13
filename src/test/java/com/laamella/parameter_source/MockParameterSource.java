26d74e6dc752a82e101da6c382aa7f9bf30b9603
Danny van Bruggen <hexagonaal@gmail.com>
2017-02-13 17:04:59 +0100

    It's easy if this works on strings

--- src/test/java/com/laamella/parameter_source/MockParameterSource.java
+++ src/test/java/com/laamella/parameter_source/MockParameterSource.java
@@ -2,14 +2,11 @@ package com.laamella.parameter_source;
 
 import java.util.Optional;
 
-public class MockParameterSource extends ParameterSource {
-    @Override
-    public Optional<String> getOptionalString(String key) {
-        return null;
-    }
+public class MockParameterSource extends StringParameterSource {
+    public Optional<String> value;
 
     @Override
-    public Optional<Integer> getOptionalInteger(String key) {
-        return null;
+    public Optional<String> getOptionalString(String key) {
+        return value;
     }
 }
